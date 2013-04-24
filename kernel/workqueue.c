24da2b4a02bd3623848aacc8aec3e37a3bc4ecc8
Viresh Kumar <viresh.kumar@linaro.org>
2013-04-24 17:12:54 +0530

    [LINARO] workqueue: Add system wide power_efficient workqueues
    Adapted for 3.4 from lsk-v3.10
    
    This patch adds system wide workqueues aligned towards power saving. This is
    done by allocating them with WQ_UNBOUND flag if 'wq_power_efficient' is set to
    'true'.
    
    tj: updated comments a bit.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    (cherry picked from commit 0668106ca3865ba945e155097fb042bf66d364d3)
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>
    
    Conflicts:
    	include/linux/workqueue.h
    
    Conflicts:
    	include/linux/workqueue.h
    	kernel/workqueue.c

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -314,6 +314,8 @@ EXPORT_SYMBOL(system_wq);
 struct workqueue_struct *system_highpri_wq __read_mostly;
 EXPORT_SYMBOL_GPL(system_highpri_wq);
 struct workqueue_struct *system_long_wq __read_mostly;
+struct workqueue_struct *system_power_efficient_wq __read_mostly;
+struct workqueue_struct *system_freezable_power_efficient_wq __read_mostly;
 EXPORT_SYMBOL_GPL(system_long_wq);
 struct workqueue_struct *system_unbound_wq __read_mostly;
 EXPORT_SYMBOL_GPL(system_unbound_wq);
@@ -323,6 +325,8 @@ EXPORT_SYMBOL_GPL(system_freezable_wq);
 static int worker_thread(void *__worker);
 static void copy_workqueue_attrs(struct workqueue_attrs *to,
 				 const struct workqueue_attrs *from);
+EXPORT_SYMBOL_GPL(system_power_efficient_wq);
+EXPORT_SYMBOL_GPL(system_freezable_power_efficient_wq);
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/workqueue.h>
@@ -5057,8 +5061,14 @@ static int __init init_workqueues(void)
 					    WQ_UNBOUND_MAX_ACTIVE);
 	system_freezable_wq = alloc_workqueue("events_freezable",
 					      WQ_FREEZABLE, 0);
+	system_power_efficient_wq = alloc_workqueue("events_power_efficient",
+					      WQ_POWER_EFFICIENT, 0);
+	system_freezable_power_efficient_wq = alloc_workqueue("events_freezable_power_efficient",
+					      WQ_FREEZABLE | WQ_POWER_EFFICIENT,
+					      0);
 	BUG_ON(!system_wq || !system_highpri_wq || !system_long_wq ||
-	       !system_unbound_wq || !system_freezable_wq);
+	       !system_unbound_wq || !system_freezable_wq) ||
+	       !system_power_efficient_wq || !system_freezable_power_efficient_wq);
 	return 0;
 }
 early_initcall(init_workqueues);
