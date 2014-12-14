2e38040fe9a52f3964056bb25ed21e2f130e70d7
psndna88 <psndna88@gmail.com>
2014-12-14 16:26:10 +0530

    selinux: set to permissive "permanently" (arter97)
    Set selinux_enforcing to 0 and disable userspace write function.
    
    Ramdisk won't be able to turn SELinux back to "enforcing"
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -136,6 +136,7 @@ static ssize_t sel_read_enforce(struct file *filp, char __user *buf,
 	return simple_read_from_buffer(buf, count, ppos, tmpbuf, length);
 }
 
+#ifndef CONFIG_SECURITY_SELINUX_FORCE_PERMISSIVE
 #ifdef CONFIG_SECURITY_SELINUX_DEVELOP
 static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 				 size_t count, loff_t *ppos)
@@ -190,10 +191,15 @@ out:
 #else
 #define sel_write_enforce NULL
 #endif
+#endif
 
 static const struct file_operations sel_enforce_ops = {
 	.read		= sel_read_enforce,
+#ifdef CONFIG_SECURITY_SELINUX_FORCE_PERMISSIVE
+	.write		= NULL,
+#else
 	.write		= sel_write_enforce,
+#endif
 	.llseek		= generic_file_llseek,
 };
 
