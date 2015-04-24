704b9f26cfcfffcd95c36419a9d2189e35081d6f
Brian Smith <b671983@gmail.com>
2015-04-23 20:48:05 -0400

    Revert "msm: kgsl: prevent thermal-engine from lowering gpu frequency if"
    This reverts commit 72e567960ea37d31796b1442720fbe726c55f5e0.

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -44,8 +44,6 @@
 #define INIT_UDELAY		200
 #define MAX_UDELAY		2000
 
-extern bool full_fm;
-
 struct clk_pair {
 	const char *name;
 	uint map;
@@ -395,13 +393,7 @@ static ssize_t kgsl_pwrctrl_max_gpuclk_store(struct device *dev,
 	if (level < 0)
 		goto done;
 
-	if (likely(full_fm))
-		pwr->thermal_pwrlevel = (unsigned int) level;
-	else if (!strcmp(current->comm, "thermal-engine") &&
-		level > pwr->thermal_pwrlevel && (level == 1 || level == 2))
-		pr_info("%s: prevented %s from setting thermal_pwrlevel %d\n",
-			__func__, current->comm, level);
-	else pwr->thermal_pwrlevel = (unsigned int) level;
+	pwr->thermal_pwrlevel = (unsigned int) level;
 
 	/*
 	 * if the thermal limit is lower than the current setting,
